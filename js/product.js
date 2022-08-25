f805c18e02db6927d410cd9b8e537364a8133ebc
martemo <marte.moslet@hotmail.com>
2022-08-25 19:58:28 +0200

    update products

---
+++ js/product.js
@@ -0,0 +1,15 @@
+const baseUrl = "https://rainydays.martemoslet.one/wp-json/wc/store/products";
+const productContainer = document.querySelector(".products");
+
+async function getProducts(url){
+    const response = await fetch(url);
+    const products = await response.json();
+    products.forEach(function(product){
+        productContainer.innerHTML += `
+        <div class="product"><h3>${product.name}</h3>
+        <div class="product-image" style="background-image: url('${product.images[0].src}')">
+        </div>`
+    })
+}
+
+getProducts(baseUrl);
