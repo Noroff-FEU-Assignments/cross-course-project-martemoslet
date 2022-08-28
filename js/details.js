3cf51b505c891ea208db762fa3b9ef4bfcf90c1e
martemo <marte.moslet@hotmail.com>
2022-08-28 16:05:56 +0200

    Update jacket page

--- js/details.js
+++ js/details.js
@@ -1,6 +1,9 @@
 const details = document.querySelector(".details");
 const queryString = document.location.search;
-const jacketsimages = document.querySelector(".jacketsimages");
+
+const cartList = document.querySelector(".cart-list");
+const totalContainer = document.querySelector(".total");
+let cartArray = [];
 
 const params = new URLSearchParams(queryString);
 
@@ -13,14 +16,15 @@ async function getData() {
         const response = await fetch(url);
         const product = await response.json();
         details.innerHTML = "";
-        jacketsimages.innerHTML = "";
 
         details.innerHTML = `<h1>${product.name}</h1>
                             <div class="details-image" style="background-image: url('${product.images[0].src}')"></div>
                             <ul>
                             <li>${product.description}</li>
                             <li>${product.prices.price} ${product.prices.currency_code}</li>
-                            </ul>`
+                            </ul>
+                            <button class="product-button cta-cart" data-product="${product.id}"">Add to cart</button>
+                            `
     }
     
    catch(error) {
@@ -30,4 +34,29 @@ async function getData() {
 }
 
 getData();
+
+const button = document.querySelector("button");
+
+    button.onclick = function(event){
+        const itemToAdd = productArray.find(item => item.id === event.target.dataset.product);
+        cartArray.push(itemToAdd);
+        showCart(cartArray);
+        localStorage.setItem("cartList", JSON.stringify(cartArray));
+    }
+
+function showCart(cartItems){
+    cart.style.display = "block";
+    cartList.innerHTML = "";
+    let total = 0;
+    cartItems.forEach(function(cartElement){
+        total += cartElement.price;
+        cartList.innerHTML +=
+        `<div class="cart-item">
+        <h4>${cartElement.name}</h4>
+        <img src="images/jacket.jpg" alt="Jacket" class="cart-image" />
+        </div> 
+        `
+    })
+    totalContainer.innerHTML = `Total: ${total}`;
+}
 
--- js/product.js
+++ js/product.js
@@ -6,7 +6,7 @@ async function getProducts(url){
     const products = await response.json();
     products.forEach(function(product){
         productContainer.innerHTML += `
-        <a href="products.html?id=${product.id}" class="card">
+        <a href="products.html?id=${product.id}">
         <div class="product"><h3>${product.name}</h3>
         <div class="product-image" style="background-image: url('${product.images[0].src}')">
         </div>
